385bdf4c9bc07718dbc7ce08f211b5c87398b116
ruz145 <wanyonyi_andrew@yahoo.com>
2024-04-26 05:14:32 +0300

    fixed errors

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About
+  return ( 
+  <div id = "about">
     <h2> About Me </h2>
-    <p> I am a developer from Nairobi</p>
-    <img src={image} alt="I made this" />
-  </div>;
+    <p> I made this </p>
+    <img src = { image } alt = "I made this" />
+  </div>
+  );
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,12 @@ import React from "react";
 
 function NavBar() {
   
-  return <nav>NavBar
-    <a href = "#home"> I'm a link!</a>
-    <a href = "#about"> I made this</a>
-  </nav>;
+  return (
+  <nav>
+    <a href = "#home"> Home</a>
+    <a href = "#about"> About</a>
+  </nav>
+  );
 }
 
 export default NavBar;
