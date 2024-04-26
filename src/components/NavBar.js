db49ecbca33e378a838570ef407c7cffc9436153
ruz145 <wanyonyi_andrew@yahoo.com>
2024-04-26 04:43:28 +0300

    added items to the nav element

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return <nav>NavBar
+    <a href = "#home"> I'm a link!</a>
+    <a href = "#about"> I made this</a>
+  </nav>;
 }
 
 export default NavBar;
