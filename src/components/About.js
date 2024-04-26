952ded7e5f2b6e28e46c15c1e0b5aa5a5d4a832e
ruz145 <wanyonyi_andrew@yahoo.com>
2024-04-26 05:08:21 +0300

    new changes

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div>About
+    <h2> About Me </h2>
+    <p> I am a developer from Nairobi</p>
+    <img src={image} alt="I made this" />
+  </div>;
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function NavBar() {
-  // update the JSX being returned!
+  
   return <nav>NavBar
     <a href = "#home"> I'm a link!</a>
     <a href = "#about"> I made this</a>
